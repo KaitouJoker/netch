ba4a40d7c9f0defdf48e16181a0061797035376a
KaitouJoker <aksdhksrud7@gmail.com>
2026-09-12 05:26:59 +0900

    perf(tcp): optimize TCPHandler with SRWLOCK, 64KB buffers, TCP_NODELAY, 2MB socket buffers, and clean shutdown

--- Redirector/TCPHandler.cpp
+++ Redirector/TCPHandler.cpp
@@ -3,23 +3,23 @@
 SOCKET tcpSocket = INVALID_SOCKET;
 USHORT tcpListen = 0;
 
-mutex tcpLock;
-map<USHORT, SOCKADDR_IN6> tcpContext;
+static SRWLOCK tcpLock = SRWLOCK_INIT;
+static unordered_map<USHORT, SOCKADDR_IN6> tcpContext;
 
 bool TCPHandler::INIT()
 {
-	auto lg = lock_guard<mutex>(tcpLock);
+	AcquireSRWLockExclusive(&tcpLock);
 
 	if (tcpSocket != INVALID_SOCKET)
 	{
 		closesocket(tcpSocket);
-
 		tcpSocket = INVALID_SOCKET;
 	}
 
 	auto client = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
 	if (client == INVALID_SOCKET)
 	{
+		ReleaseSRWLockExclusive(&tcpLock);
 		printf("[Redirector][TCPHandler::INIT] Create socket failed: %d\n", WSAGetLastError());
 		return false;
 	}
@@ -29,11 +29,20 @@ bool TCPHandler::INIT()
 		if (setsockopt(client, IPPROTO_IPV6, IPV6_V6ONLY, (char*)&v6only, sizeof(v6only)) == SOCKET_ERROR)
 		{
 			printf("[Redirector][TCPHandler::INIT] Set socket option failed: %d\n", WSAGetLastError());
-
 			closesocket(client);
+			ReleaseSRWLockExclusive(&tcpLock);
 			return false;
 		}
 	}
+
+	// Disable Nagle on listen socket
+	int nodelay = 1;
+	setsockopt(client, IPPROTO_TCP, TCP_NODELAY, (char*)&nodelay, sizeof(nodelay));
+
+	// Expand socket buffer sizes to 2MB
+	int bufSize = 2 * 1024 * 1024;
+	setsockopt(client, SOL_SOCKET, SO_RCVBUF, (char*)&bufSize, sizeof(bufSize));
+	setsockopt(client, SOL_SOCKET, SO_SNDBUF, (char*)&bufSize, sizeof(bufSize));
 
 	{
 		SOCKADDR_IN6 addr;
@@ -42,8 +51,8 @@ bool TCPHandler::INIT()
 		if (bind(client, (PSOCKADDR)&addr, sizeof(SOCKADDR_IN6)) == SOCKET_ERROR)
 		{
 			printf("[Redirector][TCPHandler::INIT] Bind socket failed: %d\n", WSAGetLastError());
-
 			closesocket(client);
+			ReleaseSRWLockExclusive(&tcpLock);
 			return false;
 		}
 	}
@@ -51,8 +60,8 @@ bool TCPHandler::INIT()
 	if (listen(client, 1024) == SOCKET_ERROR)
 	{
 		printf("[Redirector][TCPHandler::INIT] Listen socket failed: %d\n", WSAGetLastError());
-
 		closesocket(client);
+		ReleaseSRWLockExclusive(&tcpLock);
 		return false;
 	}
 
@@ -62,8 +71,8 @@ bool TCPHandler::INIT()
 		if (getsockname(client, (PSOCKADDR)&addr, &addrLength) == SOCKET_ERROR)
 		{
 			printf("[Redirector][TCPHandler::INIT] Get listen address failed: %d\n", WSAGetLastError());
-
 			closesocket(client);
+			ReleaseSRWLockExclusive(&tcpLock);
 			return false;
 		}
 
@@ -71,6 +80,7 @@ bool TCPHandler::INIT()
 	}
 
 	tcpSocket = client;
+	ReleaseSRWLockExclusive(&tcpLock);
 
 	thread(TCPHandler::Accept).detach();
 	return true;
@@ -78,41 +88,41 @@ bool TCPHandler::INIT()
 
 void TCPHandler::FREE()
 {
-	auto lg = lock_guard<mutex>(tcpLock);
+	AcquireSRWLockExclusive(&tcpLock);
 
 	if (tcpSocket != INVALID_SOCKET)
 	{
 		closesocket(tcpSocket);
-
 		tcpSocket = INVALID_SOCKET;
 	}
 	tcpListen = 0;
 
 	tcpContext.clear();
+	ReleaseSRWLockExclusive(&tcpLock);
 }
 
 void TCPHandler::CreateHandler(SOCKADDR_IN6 client, SOCKADDR_IN6 remote)
 {
-	auto lg = lock_guard<mutex>(tcpLock);
-
 	auto id = (client.sin6_family == AF_INET) ? ((PSOCKADDR_IN)&client)->sin_port : client.sin6_port;
-	if (tcpContext.find(id) != tcpContext.end())
-		tcpContext.erase(id);
 
+	AcquireSRWLockExclusive(&tcpLock);
 	tcpContext[id] = remote;
+	ReleaseSRWLockExclusive(&tcpLock);
 }
 
 void TCPHandler::DeleteHandler(SOCKADDR_IN6 client)
 {
-	auto lg = lock_guard<mutex>(tcpLock);
-
 	auto id = (client.sin6_family == AF_INET) ? ((PSOCKADDR_IN)&client)->sin_port : client.sin6_port;
-	if (tcpContext.find(id) != tcpContext.end())
-		tcpContext.erase(id);
+
+	AcquireSRWLockExclusive(&tcpLock);
+	tcpContext.erase(id);
+	ReleaseSRWLockExclusive(&tcpLock);
 }
 
 void TCPHandler::Accept()
 {
+	SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_ABOVE_NORMAL);
+
 	while (tcpSocket != INVALID_SOCKET)
 	{
 		auto client = accept(tcpSocket, NULL, NULL);
@@ -126,12 +136,23 @@ void TCPHandler::Accept()
 			return;
 		}
 
+		// Disable Nagle's algorithm immediately on client socket
+		int nodelay = 1;
+		setsockopt(client, IPPROTO_TCP, TCP_NODELAY, (char*)&nodelay, sizeof(nodelay));
+
+		// Expand client socket buffers to 2MB
+		int bufSize = 2 * 1024 * 1024;
+		setsockopt(client, SOL_SOCKET, SO_RCVBUF, (char*)&bufSize, sizeof(bufSize));
+		setsockopt(client, SOL_SOCKET, SO_SNDBUF, (char*)&bufSize, sizeof(bufSize));
+
 		thread(TCPHandler::Handle, client).detach();
 	}
 }
 
 void TCPHandler::Handle(SOCKET client)
 {
+	SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_ABOVE_NORMAL);
+
 	USHORT id = 0;
 
 	{
@@ -147,29 +168,42 @@ void TCPHandler::Handle(SOCKET client)
 		id = (addr.sin6_family == AF_INET) ? ((PSOCKADDR_IN)&addr)->sin_port : addr.sin6_port;
 	}
 
-	tcpLock.lock();
-	if (tcpContext.find(id) == tcpContext.end())
-	{
-		tcpLock.unlock();
+	SOCKADDR_IN6 target{};
+	bool found = false;
 
+	AcquireSRWLockShared(&tcpLock);
+	auto it = tcpContext.find(id);
+	if (it != tcpContext.end())
+	{
+		target = it->second;
+		found = true;
+	}
+	ReleaseSRWLockShared(&tcpLock);
+
+	if (!found)
+	{
 		closesocket(client);
 		return;
 	}
-
-	auto target = tcpContext[id];
-	tcpLock.unlock();
 
 	auto remote = new SocksHelper::TCP();
 	if (!remote->Connect(&target))
 	{
 		closesocket(client);
-
 		delete remote;
 		return;
 	}
 
-	thread(TCPHandler::Send, client, remote).detach();
+	thread sendThread(TCPHandler::Send, client, remote);
 	TCPHandler::Read(client, remote);
+
+	// Graceful shutdown to wake up sendThread immediately without UAF
+	shutdown(client, SD_BOTH);
+	if (remote->tcpSocket != INVALID_SOCKET)
+		shutdown(remote->tcpSocket, SD_BOTH);
+
+	if (sendThread.joinable())
+		sendThread.join();
 
 	closesocket(client);
 	delete remote;
@@ -177,30 +211,46 @@ void TCPHandler::Handle(SOCKET client)
 
 void TCPHandler::Read(SOCKET client, SocksHelper::PTCP remote)
 {
-	char buffer[1446];
+	SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_ABOVE_NORMAL);
+	char buffer[65536];
 	
 	while (tcpSocket != INVALID_SOCKET)
 	{
 		int length = remote->Read(buffer, sizeof(buffer));
 		if (length == 0 || length == SOCKET_ERROR)
+		{
+			shutdown(client, SD_BOTH);
 			return;
+		}
 
 		if (send(client, buffer, length, 0) != length)
+		{
+			shutdown(client, SD_BOTH);
 			return;
+		}
 	}
 }
 
 void TCPHandler::Send(SOCKET client, SocksHelper::PTCP remote)
 {
-	char buffer[1446];
+	SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_ABOVE_NORMAL);
+	char buffer[65536];
 
 	while (tcpSocket != INVALID_SOCKET)
 	{
 		int length = recv(client, buffer, sizeof(buffer), 0);
 		if (length == 0 || length == SOCKET_ERROR)
+		{
+			if (remote->tcpSocket != INVALID_SOCKET)
+				shutdown(remote->tcpSocket, SD_BOTH);
 			return;
+		}
 
 		if (remote->Send(buffer, length) != length)
+		{
+			if (remote->tcpSocket != INVALID_SOCKET)
+				shutdown(remote->tcpSocket, SD_BOTH);
 			return;
+		}
 	}
 }
